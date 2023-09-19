4f2da9c1e2c381f19417ec328390d1563fff73c8
Vasily Akimtsev <akimtcev@mail.ru>
2023-09-19 11:09:40 +0300

    initial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        }
+    }
