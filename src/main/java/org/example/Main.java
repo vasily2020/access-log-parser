df098e6b180abd56936fe48fea35f9535fbe6df0
Vasily Akimtsev <akimtcev@mail.ru>
2023-10-01 12:42:24 +0300

    AccessParserLog update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //System.out.println("Случайное число от 0 до 1: " + Math.random());
         System.out.println("Случайное число от 0 до 1: " + Math.random());
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         }
