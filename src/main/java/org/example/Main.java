a4aedde58ddf0695efb20a46897c476a5d5c34aa
Vasily Akimtsev <akimtcev@mail.ru>
2023-10-04 17:55:41 +0300

    AccessParserLog update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Случайное число от 0 до 1: " + Math.random());
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        System.out.println("Vvedite chislo:");
+        int first_number = new Scanner(System.in).nextInt();
+        System.out.println("Vvedite chislo:");
+        int second_number = new Scanner(System.in).nextInt();
+        int summa = first_number + second_number ;
+        int raznost = first_number - second_number ;
+        int proizvedenie = first_number * second_number ;
+        int chastnoe = 0;
+        System.out.println("summa dvuh chisel ravna " + summa);
+        System.out.println("raznost dvuh chisel ravna " + raznost);
+        System.out.println("proizvedenie dvuh chisel ravna " + proizvedenie);
+
         }
     }
