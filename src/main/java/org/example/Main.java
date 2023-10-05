d51ef84e1d27fa01b643a28f0e16fec6acae8afa
Vasily Akimtsev <akimtcev@mail.ru>
2023-10-05 15:04:13 +0300

    AccessParserLog update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,11 +11,12 @@ public class Main {
         int second_number = new Scanner(System.in).nextInt();
         int summa = first_number + second_number ;
         int raznost = first_number - second_number ;
-        int proizvedenie = first_number * second_number ;
-        int chastnoe = 0;
+        int proizvedenie =  first_number * second_number ;
+        double chastnoe = (double) first_number / second_number ;
         System.out.println("summa dvuh chisel ravna " + summa);
         System.out.println("raznost dvuh chisel ravna " + raznost);
-        System.out.println("proizvedenie dvuh chisel ravna " + proizvedenie);
+        System.out.println("proizvedenie dvuh chisel ravno " + proizvedenie);
+        System.out.println("chastnoe dvuh chisel ravno " + chastnoe);
 
         }
     }
